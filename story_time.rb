8349c6866ccb3cea14c38431acf751c851778a20
JRisJunior <jeffmrubin@gmail.com>
2022-11-28 14:18:46 -0600

    add Mr. Brady meets Mrs. Brady

--- story_time.rb
+++ story_time.rb
@@ -1,11 +1,16 @@
-Here's the story of a lovely lady
+Heres the story of a lovely lady
 Who was bringing up three very lovely girls.
 All of them had hair of gold, like their mother,
 The youngest one in curls.
 
-It's the story
+Its the story
 Of a man named Brady
 Who was busy with three boys of his own
 They were four men
 Living all together
 Yet they were all alone
+
+Til the one day when the lady met this fellow
+And they knew that it was much more than a hunch
+That this group must somehow form a family
+Thats the way we all became the Brady bunch
