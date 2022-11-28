a5b9928939134b183194f6806208cdd5b6e6ba0d
JRisJunior <jeffmrubin@gmail.com>
2022-11-28 14:16:41 -0600

    create Mr. Brady and family

--- story_time.rb
+++ story_time.rb
@@ -2,3 +2,10 @@ Here's the story of a lovely lady
 Who was bringing up three very lovely girls.
 All of them had hair of gold, like their mother,
 The youngest one in curls.
+
+It's the story
+Of a man named Brady
+Who was busy with three boys of his own
+They were four men
+Living all together
+Yet they were all alone
